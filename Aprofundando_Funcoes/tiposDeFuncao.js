0ef466a0473f93e2a9b5cf9ee1fc6aec08bd2fe5
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-12-05 00:37:16 -0300

    Create tiposDeFuncao.js

---
+++ Aprofundando_Funcoes/tiposDeFuncao.js
@@ -0,0 +1,22 @@
+//Tipos de funções
+
+//declarativas
+function funcao1(){
+    console.log('Sou uma mensagem de uma função declarativa')
+}
+funcao1();
+
+// expressão de função
+// com nomeação
+var funcao2 = function(){
+    console.log('Sou uma mensagem de função de expressão')
+}
+funcao2();
+
+//arrow function
+
+var funcao3 = () => {
+    console.log('Sou uma arrow function')
+
+}
+funcao3();
